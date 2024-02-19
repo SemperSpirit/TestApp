20f679ec4a6781d73fcc1597f2dcdc3407fed085
SemperSpirit <kaiten-aiko@mail.ru>
2024-02-19 21:21:13 +0300

    add additions

--- TestApp/ViewController.swift
+++ TestApp/ViewController.swift
@@ -7,6 +7,17 @@
 
 import UIKit
 
+extension UITextField {
+    func setLeftIcon(_ image: UIImage) {
+        let iconView = UIImageView(frame: CGRect(x: 10, y: 5, width: 20, height: 20))
+        iconView.image = image
+        let iconContainerView: UIView = UIView(frame: CGRect(x: 100, y: 0, width: 30, height: 30))
+        iconContainerView.addSubview(iconView)
+        leftView = iconContainerView
+        leftViewMode = .always
+    }
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var loginLabel: UILabel!
@@ -21,11 +32,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var buttons: UIStackView!
     @IBOutlet weak var strip: UIView!
     @IBOutlet weak var labelConnect: UILabel!
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
+        loginButton.layer.shadowColor = UIColor.white.cgColor
+        loginButton.layer.shadowOpacity = 2
+        loginButton.layer.shadowOffset = .zero
+        loginButton.layer.shadowRadius = 20
+        loginButton.layer.shouldRasterize = true
+        loginButton.layer.rasterizationScale = UIScreen.main.scale
     }
-    @IBAction func enterInAccount(_ sender: String) {
+    
+    @IBAction func enterInAccount(_ sender: Any) {
         print("Success")
     }
 }
