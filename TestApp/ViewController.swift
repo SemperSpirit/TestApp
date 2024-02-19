7a5027c9b655d07dd9cd2e4a304c61f4667911a4
SemperSpirit <kaiten-aiko@mail.ru>
2024-02-19 18:57:35 +0300

    add textfields

--- TestApp/ViewController.swift
+++ TestApp/ViewController.swift
@@ -11,6 +11,10 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var loginLabel: UILabel!
     @IBOutlet weak var background: UIImageView!
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordIcon: UIImageView!
+    @IBOutlet weak var loginIcon: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
